caace8072121cb84d38587c2f37f3dc10708701c
capawing <35475195+capawing@users.noreply.github.com>
2019-01-20 01:34:25 +0900

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <p>This Activity diagram shows that i'm using swimlanes which divided into two partition,
 <br>the Actors(Passenger,driver,admin)and the use cases. In these Diagrams it distinguish,
 <br>capabilities,roles and responsibilities for each sub-process in application process workflows.
-<br>Acivity Diagrams help to identify and organize the steps and overall process and identify
+<br>Acivity Diagrams help to identify and organize the steps and identify
 <br>the participants into the entire Application process.<br/></p>
 </body>
 </html>
