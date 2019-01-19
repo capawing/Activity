98a15281101b3752fd65f5cdc13616c1778da10d
capawing <35475195+capawing@users.noreply.github.com>
2019-01-20 01:11:54 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+!DOCTYPE html>
+<html>
+<head>
+<title>Activity Diagram</title>
+</head>
+<body>
+<img src="Activity Diagram.jpg">
+<p>This Acivity diagram shows that i'm using swimlanes which divided into two partition,
+<br>the Actors(Passenger,driver,admin)and the use cases. In these Diagrams it distinguish,
+<br>capabilities,roles and responsibilities for each sub-process in application process workflows.
+<br>Acivity Diagrams help to identify and organize the steps and overall process and identify
+<br>the participants into the entire Application process.<br/></p>
+</body>
+</html>
